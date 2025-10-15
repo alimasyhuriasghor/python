e9630c31b1315175f4b6044007880b93e8eef250
Ali Masyhuri Asghor <130087772+alimasyhuriasghor@users.noreply.github.com>
2025-10-15 19:04:42 +0700

    Created the fourth folder tutorial and its file

---
+++ 04-mengenal-variable/Main.py
@@ -0,0 +1,11 @@
+# 4. Mengenal Variabel (Getting to Know Variable)
+
+# A variable is a place to store data
+
+x = 5
+y = 2 * x + 1
+print("The value of x:", x)
+print("The value of y:", y)
+
+my_name = "Ali Masyhuri Asghor is the most handsome man"
+print(my_name)
